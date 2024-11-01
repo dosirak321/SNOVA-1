7209a79e770a3612df38e874a744eab57393a66f
dosirak321 <ryu_dohun0318@naver.com>
2024-11-01 12:42:26 +0900

    Add files via upload

---
+++ SNOVA 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int x = 3;
+    int y = -3;
+    printf("x = %08X\n", x);
+    printf("y = %08x\n", y);
+    printf("x + y = %08X\n", x + y);
+
+    return 0;
+}
